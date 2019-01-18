d7d869cba529907d685a68878b6c070f330ce736
Frédéric De Smet <fredericdesmet@live.be>
2019-01-18 10:21:25 +0100

    Messages Layout

--- Teamstuff/Teamstuff/Controller/TeamViewController.swift
+++ Teamstuff/Teamstuff/Controller/TeamViewController.swift
@@ -39,9 +39,10 @@ class TeamViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-//        if let flowLayout = self.collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-//            flowLayout.itemSize = CGSize(width: self.collectionView.bounds.width, height: 120)
-//        }
+        if let flowLayout = self.collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            //flowLayout.itemSize = CGSize(width: self.collectionView.bounds.width, height: self.collectionView.bounds.height)
+            flowLayout.itemSize.width = self.collectionView.bounds.width
+        }
     }
 }
 
@@ -63,6 +64,18 @@ extension TeamViewController: UICollectionViewDataSource {
         let formatter = DateFormatter()
         formatter.dateFormat = "dd MMM HH:mm"
         cell.postedDateLabel.text = String(formatter.string(from: Date.init()))
+        
+        //This creates the shadows and modifies the cards a little bit
+        cell.contentView.layer.cornerRadius = 4.0
+        cell.contentView.layer.borderWidth = 1.0
+        cell.contentView.layer.borderColor = UIColor.clear.cgColor
+        cell.contentView.layer.masksToBounds = false
+        cell.layer.shadowColor = UIColor.gray.cgColor
+        cell.layer.shadowOffset = CGSize(width: 0, height: 1.0)
+        cell.layer.shadowRadius = 4.0
+        cell.layer.shadowOpacity = 1.0
+        cell.layer.masksToBounds = false
+        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
         
         return cell
     }
